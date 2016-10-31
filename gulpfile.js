3b14269b123c3980b7ff70ce494d4de8428c899c
Harry Lachenmayer <harrylachenmayer@gmail.com>
2016-10-31 22:38:33 +0000

    basic html

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
+const file = require('gulp-file')
 const gulp = require('gulp')
 const mkdirp = require('mkdirp')
 const path = require('path')
 const postcss = require('gulp-postcss')
 const precss = require('precss')
 const rimraf = require('rimraf')
+
+const template = require('./template')
+
+gulp.task('html', () =>
+  file('index.html', template(), {src: true})
+    .pipe(gulp.dest('./build'))
+)
 
 gulp.task('styles', () =>
   gulp.src('./styles/*.css')
@@ -25,13 +33,15 @@ gulp.task('clean', (done) => {
 
 gulp.task('watch', () => {
   const watchers = [
-    gulp.watch('./styles/*.css', gulp.parallel('styles'))
+    gulp.watch('./styles/*.css', gulp.parallel('styles')),
+    gulp.watch('./*.js', gulp.parallel('html')),
   ]
   watchers.forEach(watcher => {
-    watcher.on('change', event => {
-      console.log(event)
-    })
+    watcher.on('change', event => { console.log(event) })
   })
 })
 
-gulp.task('develop', gulp.series('clean', 'watch'))
+gulp.task('build', gulp.parallel('html', 'styles'))
+
+gulp.task('release', gulp.series('clean', 'build'))
+gulp.task('develop', gulp.series('clean', 'build', 'watch'))
---
+++ template.js
@@ -0,0 +1,12 @@
+module.exports = () => `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    nothing to see here
+  </body>
+</html>
+`
